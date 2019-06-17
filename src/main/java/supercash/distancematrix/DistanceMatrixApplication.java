2b167532aade3b37abf10cdf7ebc575dc5fb2b9b
Leandro Melo de Sales <leandromsales@gmail.com>
2019-06-17 16:04:39 -0300

    First version of supercash distance matrix service

---
+++ src/main/java/supercash/distancematrix/DistanceMatrixApplication.java
@@ -0,0 +1,12 @@
+package supercash.distancematrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistanceMatrixApplication {
+
+	public static void main(String args[]) {
+		SpringApplication.run(DistanceMatrixApplication.class, args);
+	}
+}
