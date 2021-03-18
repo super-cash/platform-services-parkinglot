718d03f537b7d62956b5a272d5a3bfd96292c774
Leandro Melo de Sales <leandromsales@gmail.com>
2021-03-18 19:52:47 -0300

    Adding JodaModule to CustomObjectMapper.

--- src/main/java/cash/super_/platform/service/configuration/json/CustomObjectMapper.java
+++ src/main/java/cash/super_/platform/service/configuration/json/CustomObjectMapper.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class CustomObjectMapper extends ObjectMapper {
@@ -15,7 +16,6 @@ public class CustomObjectMapper extends ObjectMapper {
         super();
         configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS, true);
-        configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
         configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, true);
         configure(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS, true);
@@ -23,6 +23,7 @@ public class CustomObjectMapper extends ObjectMapper {
         addMixIn(Exception.class, ExceptionMixIn.class);
         addMixIn(Throwable.class, ThrowableMixIn.class);
         registerModule(new JavaTimeModule());
+        registerModule(new JodaModule());
     }
 
     @JsonIgnoreProperties({"stackTrace", "cause", "message", "suppressed", "localizedMessage"})
