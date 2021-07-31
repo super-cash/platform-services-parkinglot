e34ddff93281077bd502ebc095b82f091075b12a
Marcello de Sales <marcello.desales@gmail.com>
2021-07-30 17:37:43 -0700

    :bug: Return 200 with ticket status of exited ticket
    When the ticket exists with error code 1 in WPS and exists in our DB, we return a status.
    The user will properly show the correct state showing that the ticket exited.

--- src/main/java/cash/super_/platform/service/parkinglot/ticket/ParkingPlusTicketStatusProxyService.java
+++ src/main/java/cash/super_/platform/service/parkinglot/ticket/ParkingPlusTicketStatusProxyService.java
@@ -10,6 +10,9 @@ import java.util.TimeZone;
 import cash.super_.platform.error.supercash.*;
 import cash.super_.platform.service.parkinglot.AbstractParkingLotProxyService;
 import cash.super_.platform.service.parkinglot.model.ParkingTicketState;
+import cash.super_.platform.service.parkinglot.model.ParkinglotTicket;
+import cash.super_.platform.service.parkinglot.repository.ParkinglotTicketRepository;
+import cash.super_.platform.service.parkinglot.repository.ParkinglotTicketStateTransitionsRepository;
 import cash.super_.platform.service.parkinglot.repository.TestingParkingLotStatusInMemoryRepository;
 import cash.super_.platform.utils.SecretsUtil;
 import com.google.common.base.Strings;
@@ -44,6 +47,15 @@ public class ParkingPlusTicketStatusProxyService extends AbstractParkingLotProxy
   @Autowired
   private ParkingPlusTicketAuthorizePaymentProxyService paymentAuthService;
 
+  @Autowired
+  private ParkinglotTicketRepository parkinglotTicketRepository;
+
+  @Autowired
+  private ParkinglotTicketStateTransitionsRepository ticketStateTransitionsRepository;
+
+  @Autowired
+  ParkingTicketsStateTransitionService parkingTicketsStateTransitionService;
+
   // Since it's only loaded in certain profiles, autowire is optional
   // https://stackoverflow.com/questions/57656119/how-to-autowire-conditionally-in-spring-boot/57656242#57656242
   @Autowired(required = false)
@@ -61,7 +73,7 @@ public class ParkingPlusTicketStatusProxyService extends AbstractParkingLotProxy
     }
 
     // load the ticket status or load a testing ticket
-    RetornoConsulta ticketStatus = null;
+    final RetornoConsulta ticketStatus;
     if (testingParkinglotTicketRepository.containsTicket(ticketNumber)) {
       LOG.debug("LOADING Query TESTING TICKET STATUS: {}", ticketNumber);
       ticketStatus = testingParkinglotTicketRepository.getQueryResult(ticketNumber);
@@ -73,80 +85,34 @@ public class ParkingPlusTicketStatusProxyService extends AbstractParkingLotProxy
     }
 
     // The ticket is not a test one... We need to retrieve it from WPS
-    long allowedExitEpoch = ParkingTicketStatus.calculateAllowedExitDateTime(ticketStatus);
-    ParkingTicketState parkingTicketState = calculateTicketStatus(throwExceptionWhileValidating, ticketStatus, allowedExitEpoch);
+    long allowedExitEpoch = !ticketExitedParkingLot(ticketStatus)
+            ? ParkingTicketStatus.calculateAllowedExitDateTime(ticketStatus)
+            : 0;
+    ParkingTicketState parkingTicketState = parkingTicketsStateTransitionService.calculateTicketStatus(
+            throwExceptionWhileValidating, ticketStatus, allowedExitEpoch);
 
     // For the testing tickets, just set the status computed
     if (testingParkinglotTicketRepository.containsTicket(ticketNumber)) {
       return testingParkinglotTicketRepository.updateStatus(ticketNumber);
     }
 
-    // There's nothing to validate yet
-    //    if (!validate) {
-    //      return new ParkingTicketStatus(ticketStatus);
-    //    }
+    // Store the ticket state transition
+    if (!ticketExitedParkingLot(ticketStatus)) {
+      parkingTicketsStateTransitionService.saveTicketTransitionStateWhileUserInLot(ticketStatus, parkingTicketState);
+      LocalDateTime gracePeriodTime = ParkingTicketStatus.calculateGracePeriod(ticketStatus, properties.getGracePeriodInMinutes(), TIMEZONE_AMERICA_SAO_PAULO);
+      return new ParkingTicketStatus(ticketStatus, parkingTicketState, getMillis(gracePeriodTime));
+    }
 
-    LocalDateTime gracePeriodTime = ParkingTicketStatus.calculateGracePeriod(ticketStatus, properties.getGracePeriodInMinutes(), TIMEZONE_AMERICA_SAO_PAULO);
-    return new ParkingTicketStatus(ticketStatus, parkingTicketState, getMillis(gracePeriodTime));
+    // ticket exited parking lot
+    return new ParkingTicketStatus(ticketStatus, parkingTicketState, -1);
+  }
+
+  private static boolean ticketExitedParkingLot(RetornoConsulta ticketStatus) {
+    return !ticketStatus.isTicketValido() && ticketStatus.getTarifaPaga() > 0 && ticketStatus.getTarifa() == -1;
   }
 
   private static long getMillis(LocalDateTime dateTime) {
     return dateTime.atZone(ZoneId.of(TIMEZONE_AMERICA_SAO_PAULO)).toInstant().toEpochMilli();
-  }
-
-  private ParkingTicketState calculateTicketStatus(boolean throwExceptionWhileValidating, RetornoConsulta ticketStatus, long allowedExitEpoch) {
-    int ticketFee = ticketStatus.getTarifa();
-    int ticketFeePaid = ticketStatus.getTarifaPaga();
-    String message = "";
-    ParkingTicketState parkingTicketState = ParkingTicketState.NOT_PAID;
-
-    long queryEpoch = ticketStatus.getDataConsulta();
-    long entryEpoch = ticketStatus.getDataDeEntrada();
-
-    // TODO Needs to be verified because it's local time
-    LocalDateTime queryDateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(queryEpoch), TimeZone.getDefault().toZoneId());
-    LocalDateTime allowedExitDateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(allowedExitEpoch), TimeZone.getDefault().toZoneId());
-    LocalDateTime gracePeriodTime = ParkingTicketStatus.calculateGracePeriod(ticketStatus, properties.getGracePeriodInMinutes(), TIMEZONE_AMERICA_SAO_PAULO);
-
-    LOG.debug("The ticket queryTime={} allowedExitTime={} gracePeriodTime={}", queryDateTime, allowedExitDateTime, gracePeriodTime);
-
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    LOG.debug("The ticket queryTimeStamp={} allowedExitTimeStamp={} gracePeriodTimeStamp={}",
-            formatter.format(queryDateTime), formatter.format(allowedExitDateTime), formatter.format(gracePeriodTime));
-
-    if (queryDateTime.isBefore(gracePeriodTime)) {
-      LOG.debug("The ticket queryTimeStamp={} is before gracePeriodTimeStamp={} so setting state to grace period",
-              formatter.format(queryDateTime), formatter.format(gracePeriodTime));
-              message += "You can leave the parking lot until " + allowedExitDateTime.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
-      parkingTicketState = ParkingTicketState.GRACE_PERIOD;
-    }
-
-    // we should not charge the user if the fee is 0
-    if (ticketFee == 0) {
-      if (allowedExitEpoch - entryEpoch < 0) {
-        message += "Today is free.";
-        parkingTicketState = ParkingTicketState.FREE;
-      }
-
-      // Status message
-      LOG.debug(message);
-
-      SupercashAmountIsZeroException exception = new SupercashAmountIsZeroException(message);
-      exception.addField("entry_date", entryEpoch);
-      exception.addField("exit_allowed_date", allowedExitEpoch);
-      if (throwExceptionWhileValidating) throw exception;
-
-    } else {
-
-      // the user is still in the parking lot after making a payment
-      if (ticketFeePaid >= ticketFee && queryDateTime.isBefore(allowedExitDateTime)) {
-        message = "The ticket is already paid and the user is still in the parking lot (since the ststus is still != 404)";
-        LOG.debug(message);
-        parkingTicketState = ParkingTicketState.PAID;
-        if (throwExceptionWhileValidating) throw new SupercashPaymentAlreadyPaidException(message);
-      }
-    }
-    return parkingTicketState;
   }
 
   /**
@@ -169,8 +135,7 @@ public class ParkingPlusTicketStatusProxyService extends AbstractParkingLotProxy
     Span newSpan = tracer.nextSpan().name("REST https://parkingplus.com.br/2/tickets").start();
     newSpan.remoteServiceName("parkinglot");
 
-    RetornoConsulta ticketStatus;
-
+    RetornoConsulta ticketStatus = null;
     try (SpanInScope spanScope = tracer.withSpanInScope(newSpan.start())) {
       LOG.info("Requesting parking lots ticket status: {}", ticketNumber);
       try {
@@ -196,7 +161,29 @@ public class ParkingPlusTicketStatusProxyService extends AbstractParkingLotProxy
 
     } catch (RuntimeException error) {
       LOG.error("Couldn't get the status of ticket: {}", error.getMessage());
-      throw error;
+
+      // Verify if the ticket existed before by checking the third party error
+      if (error instanceof SupercashThirdPartySystemException) {
+        SupercashThirdPartySystemException thirdPartySystemException = (SupercashThirdPartySystemException)error;
+
+        // Save the state transition error because the ticket existed before and the user left the parking lot
+        // When that happens, the error_code from WPS is 1. In this case, the response should be 200.
+        // https://gitlab.com/supercash/services/parking-lot-service/-/issues/2
+        Object thirdPartyErrorCode = thirdPartySystemException.SupercashExceptionModel.getAdditionalFields().get("third_party_error_code");
+        if (thirdPartyErrorCode != null && Integer.valueOf(thirdPartyErrorCode.toString()) == 1) {
+          // The ticket actually was just removed but existed before, so it can't fail
+          ParkinglotTicket savedTicket = parkingTicketsStateTransitionService.saveTicketTransitionStateAfterUserExits(ticketNumber);
+
+          // Make a new ticket status with the current status
+          ticketStatus = parkingTicketsStateTransitionService.makeNewTicketQueryAfterUserExits(savedTicket);
+
+        } else {
+          LOG.error("Ticket error from WPS error code for ticket={} errorCode={}", ticketNumber, thirdPartyErrorCode);
+          throw error;
+        }
+
+        // throw any other error, that's not error code = 1 or 3 already handled
+      } else throw error;
 
     } finally {
       newSpan.finish();
