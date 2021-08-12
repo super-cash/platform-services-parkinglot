12dbdf9487d1dc6042ec68456e8088c372d820de
Marcello de Sales <marcello.desales@gmail.com>
2021-08-12 14:18:46 -0700

    :bug: :pin: Downgrade to Junit 4 as tests don't work with 5

--- src/test/java/cash/super_/platform/service/parkinglot/ApplicationContextLoadTest.java
+++ src/test/java/cash/super_/platform/service/parkinglot/ApplicationContextLoadTest.java
@@ -1,17 +1,16 @@
 package cash.super_.platform.service.parkinglot;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-@AutoConfigureMockMvc
-@DisplayName("Basic Smoke tests")
 public class ApplicationContextLoadTest {
 
   @Test
-  @DisplayName("Load Context Test")
   public void contextLoads() {
   }
+
 }
